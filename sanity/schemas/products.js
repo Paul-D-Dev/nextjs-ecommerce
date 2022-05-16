4456355238e9c8164225f59ea3eed180eac702d5
Paul DAO <dao.van.paul@gmail.com>
2022-05-16 17:15:02 -0400

    (Sanity): create schemas products and banner

---
+++ sanity/schemas/products.js
@@ -0,0 +1,40 @@
+export default {
+    name: 'products',
+    title: 'Products',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'name',
+            type: 'string'
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 96
+            }
+        },
+        {
+            name: 'details',
+            title: 'Details',
+            type: 'string'
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{type: 'image'}],
+            options: {
+                hotspot: true
+            }
+        },
+    ],
+}
