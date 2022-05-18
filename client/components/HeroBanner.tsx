d670ed9d1b8608ccb3cae45a1888bad1c505402b
Paul DAO <dao.van.paul@gmail.com>
2022-05-18 10:43:54 -0400

    (Client) feat(HeroBanner): pass props to dynamic data

--- client/components/HeroBanner.tsx
+++ client/components/HeroBanner.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
 import Link from 'next/link';
+import {Banner} from "../_models/banner.model";
 import styles from '../styles/HeroBanner.module.scss';
+import {urlFor} from '../lib/sanity-client';
 
-const HeroBanner = () => {
+type Props = {
+    heroBanner: Banner
+}
+
+const HeroBanner: FunctionComponent<Props> = ({heroBanner}) => {
     return (
         <div className={styles.hero_banner_container}>
             <div>
-                <p className={styles.beats_solo}>beat solo</p>
-                <h3>Mid text</h3>
-                <img src="" alt="headphones" className={styles.hero_banner_image}/>
+                <p className={styles.beats_solo}>{heroBanner.smallText}</p>
+                <h3>{heroBanner.midText}</h3>
+                <img src={urlFor(heroBanner.image).url()}
+                     alt="headphones"
+                     className={styles.hero_banner_image}
+                />
             </div>
 
             <div>
-                <Link href='/product/ID'>
-                    <button type='button'>BTN TEXT</button>
+                <Link href={`/product/${heroBanner.product}`}>
+                    <button type='button'>{heroBanner.buttonText}</button>
                 </Link>
                 <div className="desc">
                     <h5>Description</h5>
-                    <p>DESCRIPTION</p>
+                    <p>{heroBanner.desc}</p>
                 </div>
             </div>
         </div>
