447709fa6ca1cbe58dc2e9fa431de4c4b217b0da
Paul DAO <dao.van.paul@gmail.com>
2022-05-22 22:47:35 -0400

    (Client)-feat(Product): implements product component

--- client/components/Product.tsx
+++ client/components/Product.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
+import {Product as ProductModel} from '../_models/product.model';
+import Link from 'next/link';
+import {urlFor} from '../lib/sanity-client'
+import styles from '../styles/Product.module.scss';
 
-const Product = () => {
+type Props = {
+  product: ProductModel;
+};
+
+const Product: FunctionComponent<Props> = ({ product: { image, name, slug, price} }) => {
     return (
-        <div>Product</div>
+        <div>
+            <Link href={`/product/${slug.current}`}>
+                <a className={styles.product_card}>
+                    <img
+                        className={styles.product_image}
+                        src={urlFor(image && image[0]).url()}
+                        alt={name}
+                        width={250}
+                        height={250}
+                    />
+                    <p className={styles.product_name}>{name}</p>
+                    <p className={styles.product_price}>${price}</p>
+                </a>
+            </Link>
+        </div>
     );
 }
 
