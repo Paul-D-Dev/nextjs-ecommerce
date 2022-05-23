97880c17e0ced17e1244011dfc57c768d63ffe3a
Paul DAO <dao.van.paul@gmail.com>
2022-05-22 23:46:00 -0400

    (Client)-feat(Layout): implement structure & styles

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
+import Head from 'next/head';
+import {Footer, Navbar} from "./index";
+import styles from "../styles/Layout.module.scss";
 
-const Layout = () => {
+type Props = {
+    children: React.ReactNode;
+}
+
+/*
+    * children props come from _app.tsx
+* */
+const Layout: FunctionComponent<Props> = ({ children }) => {
   return (
-    <div>
-      <h1>Layout</h1>
+    <div className={styles.layout}>
+        {/*Title of page*/}
+        <Head>
+            <title>Headphones shop</title>
+        </Head>
+
+        <header>
+            <Navbar/>
+        </header>
+
+        <main className={styles.main_container}>
+            {/* HomePage from index.tsx */}
+            {children}
+        </main>
+
+        <footer>
+            <Footer/>
+        </footer>
     </div>
   );
 };
